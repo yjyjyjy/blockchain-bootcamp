90f8d29a7f04ffd7c2d29296abda27823f509f4d
Junyu Yang <junyuyang@junyus-mbp.lan>
2021-10-09 18:29:02 -0700

    update

---
+++ web3_py_simple_storage/deploy.py
@@ -0,0 +1,20 @@
+from solcx import compile_standard
+import solcx
+
+solcx.install_solc("0.6.0")
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+
+# Compile solidity
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {"outputSelection": {"*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}}},
+    },
+    solc_version="0.6.0",
+)
+
+print(compiled_sol)
